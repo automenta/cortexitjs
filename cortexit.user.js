5455ea049ac00574f2101226bdcdb9b06d44a861
sseehh <1s1e1h1@gmail.com>
2014-02-27 14:17:14 -0500

    chrome extension and experimental iframe support

---
+++ cortexit.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+//
+//Displayable Name of your script 
+// @name           Cortexit
+//
+// brief description
+// @description    Text dispersant for improved reading speed and comprehension.
+//
+//URI (preferably your own site, so browser can avert naming collisions
+// @namespace      http://automenta.com/cortexit
+//
+// Your name, userscript userid link (optional)   
+// @author         SeH
+//
+// If you want to license out
+// @license        GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html) 
+//
+//(optional) may be used by browsers to display an about link
+// @homepage       http://automenta.com/cortexit/
+//
+//Version Number
+// @version        1.0
+//
+// Urls process this user script on
+// @include        http://*
+//
+// Add any library dependencies here, so they are loaded before your script is loaded.
+//
+// @require        http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js
+//
+// @history        1.0 first version
+//
+// ==/UserScript==
+
+
+//And of course your code!!
+$(function(){
+window.alert("Hello.. My Extension processed you..");
+});
+
+//http://developer.chrome.com/extensions/samples#script
+
+//window.getSelection().anchorNode //http://stackoverflow.com/questions/18960808/html5-event-for-onselectionchange
+//document.getSelection()
+//
